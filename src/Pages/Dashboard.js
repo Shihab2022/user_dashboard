72ac3f85b380910c6bddeec2d34be338d5326a57
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-10-30 21:58:02 +0600

    change color in button

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -11,7 +11,7 @@ const Dashboard = () => {
      <h1 className="text-3xl font-bold tracking-tight text-gray-900">Users</h1>
      <p className='text-zinc-500 font-semibold ml-2'>37 users</p>
      </div>
-            <button className='bg-blue-700 px-3 py-1 h-12 text-lg text-white rounded-md'> Invite User</button>
+            <button className='bg-gradient-to-r from-cyan-500 to-amber-500 px-3 py-1 h-12 text-lg text-white rounded-md'> <span className='text-xl '> + </span> Invite </button>
           </div>
         </header>
         <form className='max-w-7xl mx-auto lg:px-8 sm:px-6 px-4 mt-5'>   
@@ -20,8 +20,8 @@ const Dashboard = () => {
         <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
             <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
         </div>
-        <input type="search" id="default-search" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required/>
-        <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+        <input type="search" id="default-search" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search User Here ...." required/>
+        <button type="submit" className="text-white absolute right-2.5 bottom-2.5 font-medium rounded-lg text-sm px-4 py-2  bg-gradient-to-r from-cyan-500 to-amber-500">Search</button>
     </div>
 </form>
   <Main/>
