8f87f3e353196981459b5780b2c2219e9958faed
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-10-31 12:09:47 +0600

    start layout design

--- src/Chart/ChartDashboard.js
+++ src/Chart/ChartDashboard.js
@@ -1,52 +1,21 @@
-import React from 'react';
-import {Area, AreaChart, CartesianGrid, Tooltip, XAxis, YAxis } from 'recharts';
-import useGetChartData from '../hooks/GetChartData';
-
-
-
+import React from "react";
 
 const ChartDashboard = () => {
-   const [chartData]=useGetChartData()
- 
-    return (
-        <AreaChart
-        width={600}
-        height={500}
-        data={chartData}
-        margin={{
-          top: 10,
-          right: 30,
-          left: 0,
-          bottom: 0
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="month" />
-        <YAxis />
-        <Tooltip />
-        <Area
-          type="monotone"
-          dataKey="investment"
-          stackId="1"
-          stroke="#8884d8"
-          fill="#8884d8"
-        />
-        <Area
-          type="monotone"
-          dataKey="revenue"
-          stackId="1"
-          stroke="#82ca9d"
-          fill="#82ca9d"
-        />
-        <Area
-          type="monotone"
-          dataKey="sell"
-          stackId="1"
-          stroke="#ffc658"
-          fill="#ffc658"
-        />
-      </AreaChart>
-    );
+  return (
+    <>
+      <div className="grid grid-cols-12 px-14 gap-2">
+        <div className="bg-red-800 w-full h-screen p-5 col-start-1 col-end-10">
+          <div className="grid grid-cols-3 gap-5">
+            <div className="bg-white w-full rounded-md h-20"></div>
+            <div className="bg-white w-full rounded-md h-20"></div>
+            <div className="bg-white w-full rounded-md h-20"></div>
+          </div>
+        </div>
+
+        <div className="w-full h-screen col-start-10 col-end-13 bg-amber-400"></div>
+      </div>
+    </>
+  );
 };
 
 export default ChartDashboard;
