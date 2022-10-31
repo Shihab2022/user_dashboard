cd2e71e2649cbbb64f7442defdb5d9140da681f2
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-10-31 12:18:43 +0600

    complete layout design

--- src/Chart/ChartDashboard.js
+++ src/Chart/ChartDashboard.js
@@ -5,10 +5,20 @@ const ChartDashboard = () => {
     <>
       <div className="grid grid-cols-12 px-14 gap-2">
         <div className="bg-red-800 w-full h-screen p-5 col-start-1 col-end-10">
+
           <div className="grid grid-cols-3 gap-5">
             <div className="bg-white w-full rounded-md h-20"></div>
             <div className="bg-white w-full rounded-md h-20"></div>
             <div className="bg-white w-full rounded-md h-20"></div>
+          </div>
+          <div className="grid grid-cols-12 gap-2 my-5 rounded-md w-full">
+            <div className="bg-green-500 h-96 w-full col-start-1 col-end-4"></div>
+            <div className="bg-green-800 h-96 w-full col-start-4 col-end-13"></div>
+          </div>
+          <div className="grid grid-cols-3 gap-3">
+            <div className="h-64 w-full bg-zinc-400 rounded-md"></div>
+            <div className="h-64 w-full bg-zinc-400 rounded-md"></div>
+            <div className="h-64 w-full bg-zinc-400 rounded-md"></div>
           </div>
         </div>
 
